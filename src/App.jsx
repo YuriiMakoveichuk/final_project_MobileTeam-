72708f4b30931963905ec8530e02a953edb95c3a
Yurii Makoveichuk <yuriimakoveichuk@gmail.com>
2024-11-23 11:44:04 +0200

    fix app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
-import { Container } from "./components/Container/Container.jsx";
-import { Section } from "./components/Section/Section.jsx";
-
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <Section>
-        <Container>
-          <h1>hello</h1>
-        </Container>
-      </Section>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
