a8e87c7ab25149e4532a550e5c24508c46438b49
Oleksandr-Kalinin <work.kalinin.o.v@gmail.com>
2024-11-24 21:35:57 +0200

    remove component from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import UserBarPopover from "./components/UserBarPopover/UserBarPopover.jsx";
 import { SignUpPage } from "./pages/SignUpPage/SignUpPage.jsx";
 
 function App() {
   return (
     <>
       <SignUpPage />
-      <UserBarPopover />
     </>
   );
 }
