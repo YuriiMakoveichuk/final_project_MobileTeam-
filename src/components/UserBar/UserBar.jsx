78b81292c6ef61ef0f5f0dc108649790e31b8185
Oleksandr-Kalinin <work.kalinin.o.v@gmail.com>
2024-11-24 15:30:17 +0200

    add class hidden, add wrapper for svg-icon

--- src/components/UserBar/UserBar.jsx
+++ src/components/UserBar/UserBar.jsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import css from "./UserBar.module.css";
 
 const UserBar = ({ userName }) => {
@@ -9,6 +10,13 @@ const UserBar = ({ userName }) => {
         src="https://imgcdn.stablediffusionweb.com/2024/3/31/a07c234b-ab97-4ad4-96b1-e1e88ec45e45.jpg"
         alt="user's photo"
       />
+      <svg className={css.iconArrowDown}>
+        {/* <use href="../../img"></use> */}
+      </svg>
+
+      <svg className={clsx(css.iconArrowUp, "visually-hidden")}>
+        {/* <use href="../../img"></use> */}
+      </svg>
     </button>
   );
 };
