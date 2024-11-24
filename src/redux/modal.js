dc8157dac5b082998dd3ab81eab21ee1b92a43b0
Yurii Makoveichuk <yuriimakoveichuk@gmail.com>
2024-11-24 16:32:39 +0200

    added modal

---
+++ src/redux/modal.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const INITIAL_STATE = {
+  isOpen: false,
+};
+
+const modalSlice = createSlice({
+  name: "modal",
+  initialState: INITIAL_STATE,
+  reducers: {
+    openModal: (state) => {
+      state.isOpen = true;
+    },
+    closeModal: (state) => {
+      state.isOpen = false;
+    },
+  },
+});
+
+export const { openModal, closeModal } = modalSlice.actions;
+export const modalReducer = modalSlice.reducer;
