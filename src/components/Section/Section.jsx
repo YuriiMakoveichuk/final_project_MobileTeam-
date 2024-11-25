2014269f88b96fdc415d5b5afa1435ed70989f3a
OksanaLynnyk <allimpiya@gmail.com>
2024-11-25 20:40:51 +0300

    SignUpPage Added styles

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,8 +1,3 @@
-// import css from "./Section.module.css";
-
-// export const Section = ({ children }) => {
-//   return <section className={css.section}>{children}</section>;
-// };
 import css from "./Section.module.css";
 
 export const Section = ({ children }) => {
