520772b50b9a6c8066544fcc28b5003218577ddf
Oleksandr-Kalinin <work.kalinin.o.v@gmail.com>
2024-11-26 00:24:29 +0200

    add functional onClick UserBar

--- src/components/UserBar/UserBar.jsx
+++ src/components/UserBar/UserBar.jsx
@@ -1,23 +1,25 @@
-import clsx from "clsx";
 import css from "./UserBar.module.css";
 import sprite from "../../img/sprite.svg";
 
-const UserBar = ({ userName }) => {
+const UserBar = ({ clickUserBar, iconArrowUp, iconArrowDown, userName }) => {
   return (
-    <button className={css.btnUserBar} type="button">
+    <button className={css.btnUserBar} type="button" onClick={clickUserBar}>
       <p className={css.nameUserBar}>{userName}</p>
       <img
         className={css.photoUser}
         src="https://imgcdn.stablediffusionweb.com/2024/3/31/a07c234b-ab97-4ad4-96b1-e1e88ec45e45.jpg"
         alt="user's photo"
       />
-      <svg className={css.iconArrowDown}>
-        <use href={`${sprite}#icon-chevron-down`}></use>
-      </svg>
-
-      <svg className={clsx(css.iconArrowUp, "visually-hidden")}>
-        <use href={`${sprite}#icon-chevron-up`}></use>
-      </svg>
+      {iconArrowDown && (
+        <svg className={css.iconArrowDown}>
+          <use href={`${sprite}#icon-chevron-down`}></use>
+        </svg>
+      )}
+      {iconArrowUp && (
+        <svg className={css.iconArrowUp}>
+          <use href={`${sprite}#icon-chevron-up`}></use>
+        </svg>
+      )}
     </button>
   );
 };
--- src/components/UserPanel/UserPanel.jsx
+++ src/components/UserPanel/UserPanel.jsx
@@ -1,16 +1,44 @@
+import { useState } from "react";
 import UserBar from "../UserBar/UserBar.jsx";
+import UserBarPopover from "../UserBarPopover/UserBarPopover.jsx";
 
 import css from "./UserPanel.module.css";
 
 const UserPanel = () => {
   const userName = "Nadia";
 
+  const [isOpenUserBarPopover, setIsOpenUserBarPopover] = useState(false);
+  const [showIconArrowUp, setShowIconArrowUp] = useState(false);
+  const [showIconArrowDown, setShowIconArrowDown] = useState(true);
+
+  const handleClickUserBar = () => {
+    setIsOpenUserBarPopover((prevState) => !prevState);
+    setShowIconArrowUp((prevState) => !prevState);
+    setShowIconArrowDown((prevState) => !prevState);
+  };
+
+  const closeUserBarPopover = () => {
+    setIsOpenUserBarPopover(false);
+    setShowIconArrowUp(false);
+    setShowIconArrowDown(true);
+  };
+
   return (
     <div className={css.wrapperUserPanel}>
       <p className={css.titleUserPanel}>
         Hello<span className={css.nameAcceptWeight}>, {userName}!</span>
       </p>
-      <UserBar userName={userName} />
+      <div className={css.wrapperUserBar}>
+        <UserBar
+          clickUserBar={handleClickUserBar}
+          iconArrowUp={showIconArrowUp}
+          iconArrowDown={showIconArrowDown}
+          userName={userName}
+        />
+        {isOpenUserBarPopover && (
+          <UserBarPopover closeUserBarPopover={closeUserBarPopover} />
+        )}
+      </div>
     </div>
   );
 };
