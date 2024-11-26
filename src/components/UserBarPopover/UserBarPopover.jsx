c1e2342939053435580d08e3ed70531296e8c9d5
Oleksandr-Kalinin <work.kalinin.o.v@gmail.com>
2024-11-26 21:22:55 +0200

    add open click openLogOutModal, openSettingModal

--- src/components/UserBarPopover/UserBarPopover.jsx
+++ src/components/UserBarPopover/UserBarPopover.jsx
@@ -2,7 +2,20 @@ import css from "./UserBarPopover.module.css";
 import sprite from "../../img/sprite.svg";
 import { useEffect } from "react";
 
-const UserBarPopover = ({ closeUserBarPopover }) => {
+const UserBarPopover = ({
+  closeUserBarPopover,
+  openUserSettingsModal,
+  openLogOutModal,
+}) => {
+  const handleClickSetting = () => {
+    closeUserBarPopover();
+    openUserSettingsModal();
+  };
+
+  const handleClickLogOut = () => {
+    closeUserBarPopover();
+    openLogOutModal();
+  };
   useEffect(() => {
     const handleClickDown = (event) => {
       if (event.code === "Escape") {
@@ -11,7 +24,10 @@ const UserBarPopover = ({ closeUserBarPopover }) => {
     };
 
     const handleClickBackDrop = (event) => {
-      if (event.target.className !== css.wrapperUserBarPopover) {
+      const classList = [css.wrapperUserBarPopover, css.btnUserBarPopover];
+      const isClassInClassList = classList.includes(event.target.className);
+
+      if (!isClassInClassList) {
         closeUserBarPopover();
       }
     };
@@ -28,14 +44,22 @@ const UserBarPopover = ({ closeUserBarPopover }) => {
 
   return (
     <div className={css.wrapperUserBarPopover}>
-      <button className={css.btnUserBarPopover} type="button">
+      <button
+        className={css.btnUserBarPopover}
+        type="button"
+        onClick={handleClickSetting}
+      >
         <svg className={css.iconSetting} width="16" height="16">
           <use href={`${sprite}#icon-settings`}></use>
         </svg>
         Setting
       </button>
 
-      <button className={css.btnUserBarPopover} type="button">
+      <button
+        className={css.btnUserBarPopover}
+        type="button"
+        onClick={handleClickLogOut}
+      >
         <svg className={css.iconLogOut} width="16" height="16">
           <use href={`${sprite}#icon-log-out`}></use>
         </svg>
