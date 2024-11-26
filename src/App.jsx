010d2ef1103203fe2ec72809a9eab23333ab0c9f
Oleksandr-Kalinin <work.kalinin.o.v@gmail.com>
2024-11-26 21:25:08 +0200

    remove test component from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import UserPanel from "./components/UserPanel/UserPanel.jsx";
 import { SignUpPage } from "./pages/SignUpPage/SignUpPage.jsx";
 
 function App() {
   return (
     <>
       <SignUpPage />
-      <UserPanel />
     </>
   );
 }
