0f44b31489076f490434bae9526d21351098d799
Yurii Makoveichuk <yuriimakoveichuk@gmail.com>
2024-11-27 15:42:03 +0200

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import { SignUpPage } from "./pages/SignUpPage/SignUpPage.jsx";
+import { Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
+
+import Loader from "./components/Loader/Loader.jsx";
+
+const HomePage = lazy(() => import("./pages/HomePage/HomePage.jsx"));
+const SignUpPage = lazy(() => import("./pages/SignUpPage/SignUpPage.jsx"));
+const SingInPage = lazy(() => import("./pages/SingInPage/SingInPage.jsx"));
+const TrackerPage = lazy(() => import("./pages/TrackerPage/TrackerPage.jsx"));
+const NotFoundPage = lazy(() =>
+  import("./pages/NotFoundPage/NotFoundPage.jsx")
+);
 
 import "./App.css";
 
 function App() {
   return (
     <>
-      <SignUpPage />
+      <main>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route path="/signin" element={<SingInPage />} />
+            <Route path="/tracker" element={<TrackerPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </Suspense>
+      </main>
     </>
   );
 }
