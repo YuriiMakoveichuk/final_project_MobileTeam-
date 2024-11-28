5c8851ea773f1aafdac46c5cdbc1a793c9eb4caf
Kyrylko <kkuzmenko1992@gmail.com>
2024-11-28 03:04:44 -0330

    daily info

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { modalReducer } from "./modal.js";
+import { waterReducer } from "./dailyInfoSlice";
 
 export const store = configureStore({
   reducer: {
     modal: modalReducer,
+    water: waterReducer,
   },
 });
