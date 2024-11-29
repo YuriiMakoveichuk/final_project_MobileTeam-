c4f0b476b4f592abbdafd55904290efbcdf198cc
Kyrylko <kkuzmenko1992@gmail.com>
2024-11-29 00:53:49 -0330

    added swiper

--- src/components/DailyInfo/DailyInfo.jsx
+++ src/components/DailyInfo/DailyInfo.jsx
@@ -12,6 +12,13 @@ import { openModal } from "../../redux/modal";
 
 import EditModal from "../EditModal/EditModal";
 import DeleteWaterModal from "../DeleteWaterModal/DeleteWaterModal";
+import { Container } from "../Container/Container";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Scrollbar, Mousewheel } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/scrollbar";
+
 
 const DailyInfo = () => {
   const dispatch = useDispatch();
@@ -59,63 +66,81 @@ const DailyInfo = () => {
   };
 
   return (
-    <div className={`${styles.dailyInfo}`}>
-      <div className={styles.dailyHeader}>
-        <h3 className={styles.DailyInfoTitle}>Today</h3>
-        <div className={styles.addWaterWrap}>
-          <button className={styles.addWaterBtn} onClick={handleAddWater}>
-            <svg className={styles.iconAddWater} width={30} height={30}>
-              <use href={`${sprite}#icon-add`} />
-            </svg>
-          </button>
-          <p className={styles.addWaterText}>Add water</p>
+    <Container>
+      <div className={`${styles.dailyInfo}`}>
+        <div className={styles.dailyHeader}>
+          <h3 className={styles.DailyInfoTitle}>Today</h3>
+          <div className={styles.addWaterWrap}>
+            <button className={styles.addWaterBtn} onClick={handleAddWater}>
+              <svg className={styles.iconAddWater} width={30} height={30}>
+                <use href={`${sprite}#icon-add`} />
+              </svg>
+            </button>
+            <p className={styles.addWaterText}>Add water</p>
+          </div>
         </div>
-      </div>
-      <div className={styles.cardsContainer}>
-        {waterRecords.length > 0 && (
-          <ul className={styles.cardsList}>
-            {waterRecords.map((record) => (
-              <li key={record.id} className={styles.card}>
-                <div className={styles.cardContent}>
-                  <svg className={styles.glassIcon} width={38} height={38}>
-                    <use href={`${sprite}#icon-glass`} />
-                  </svg>
-                  <div className={styles.contentTextWrap}>
-                    <span className={styles.waterAmount}>
-                      {record.amount} ml
-                    </span>
-                    <span className={styles.time}>{record.time}</span>
+        <div className={styles.cardsContainer}>
+          {waterRecords.length > 0 && (
+            <Swiper
+              modules={[Mousewheel, Scrollbar]}
+              slidesPerView={2.1}
+              scrollbar={{ draggable: true }}
+              style={{ paddingBottom: "24px", zIndex: 0 }}
+              breakpoints={{
+                768: {
+                  slidesPerView: 3,
+                },
+              }}
+              mousewheel={true}
+            >
+              {waterRecords.map((record) => (
+                <SwiperSlide key={record.id} className={styles.card}>
+                  <div className={styles.cardContent}>
+                    <svg className={styles.glassIcon} width={38} height={38}>
+                      <use href={`${sprite}#icon-glass`} />
+                    </svg>
+                    <div className={styles.contentTextWrap}>
+                      <span className={styles.waterAmount}>
+                        {record.amount} ml
+                      </span>
+                      <span className={styles.time}>{record.time}</span>
+                    </div>
+                    <div className={styles.btnWrap}>
+                      <button
+                        className={styles.editBtn}
+                        onClick={() => openEditModal(record.id)}
+                      >
+                        <svg className={styles.iconEdit} width={14} height={14}>
+                          <use href={`${sprite}#icon-edit`} />
+                        </svg>
+                      </button>
+                      <button
+                        className={styles.deleteBtn}
+                        onClick={() => handleDeleteWater(record.id)}
+                      >
+                        <svg
+                          className={styles.iconTrash}
+                          width={14}
+                          height={14}
+                        >
+                          <use href={`${sprite}#icon-trash`} />
+                        </svg>
+                      </button>
+                    </div>
                   </div>
-                  <div className={styles.btnWrap}>
-                    <button
-                      className={styles.editBtn}
-                      onClick={() => openEditModal(record.id)}
-                    >
-                      <svg className={styles.iconEdit} width={14} height={14}>
-                        <use href={`${sprite}#icon-edit`} />
-                      </svg>
-                    </button>
-                    <button
-                      className={styles.deleteBtn}
-                      onClick={() => handleDeleteWater(record.id)}
-                    >
-                      <svg className={styles.iconTrash} width={14} height={14}>
-                        <use href={`${sprite}#icon-trash`} />
-                      </svg>
-                    </button>
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
-        )}
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          )}
+        </div>
+
+        {isModalOpen && modalType === "delete" && <DeleteWaterModal />}
+
+        {isModalOpen && modalType === "edit" && <EditModal />}
       </div>
-
-      {isModalOpen && modalType === "delete" && <DeleteWaterModal />}
-
-      {isModalOpen && modalType === "edit" && <EditModal />}
-    </div>
+    </Container>
   );
 };
 
 export default DailyInfo;
+
