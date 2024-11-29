a6fb5772a526a740b8f12acaa8a8a0931863d264
Justiceman81 <Justiceman814@gmail.com>
2024-11-29 19:16:11 +0200

    Section & Container rotation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+import { Route, Routes } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+
+import Loader from './components/Loader/Loader.jsx';
+
+const HomePage = lazy(() => import('./pages/HomePage/HomePage.jsx'));
+const SignUpPage = lazy(() => import('./pages/SignUpPage/SignUpPage.jsx'));
+const SingInPage = lazy(() => import('./pages/SingInPage/SingInPage.jsx'));
+const TrackerPage = lazy(() => import('./pages/TrackerPage/TrackerPage.jsx'));
+const NotFoundPage = lazy(() =>
+  import('./pages/NotFoundPage/NotFoundPage.jsx'),
+);
+
 import './App.css';
-import { SignUpPage } from './pages/SignUpPage/SignUpPage.jsx';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <main>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route path="/signin" element={<SingInPage />} />
+            <Route path="/tracker" element={<TrackerPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </Suspense>
+      </main>
+    </>
+  );
 }
 
 export default App;
