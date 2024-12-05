b68b2da3ebf5ec3643a5a6393a77d52082cc1141
Yurii Makoveichuk <yuriimakoveichuk@gmail.com>
2024-12-05 09:30:53 +0200

    merge

--- src/redux/water/dailyInfoThunk.js
+++ src/redux/water/dailyInfoThunk.js
@@ -4,7 +4,6 @@ import axios from "axios";
 // Определение URL API
 const API_URL = "https://final-project-mobileteam-backend.onrender.com/water";
 
-
 const setAuthHeaders = (token) => {
   if (token) {
     axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
@@ -13,20 +12,19 @@ const setAuthHeaders = (token) => {
   }
 };
 
-
 export const fetchWaterRecords = createAsyncThunk(
   "dailyInfo/fetchWaterRecords",
   async (_, thunkAPI) => {
     try {
-      const state = thunkAPI.getState(); 
-      const token = state.auth.token; 
+      const state = thunkAPI.getState();
+      const token = state.auth.token;
 
       console.log("Token:", token);
 
-      setAuthHeaders(token); 
+      setAuthHeaders(token);
 
-      const response = await axios.get(API_URL); 
-      return response.data; 
+      const response = await axios.get(API_URL);
+      return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(
         error.response?.data || "Failed to fetch water records"
@@ -35,18 +33,17 @@ export const fetchWaterRecords = createAsyncThunk(
   }
 );
 
-
 export const addWaterRecord = createAsyncThunk(
   "dailyInfo/addWaterRecord",
   async (newRecord, thunkAPI) => {
     try {
-      const state = thunkAPI.getState(); 
-      const token = state.auth.token; 
+      const state = thunkAPI.getState();
+      const token = state.auth.token;
 
-      setAuthHeaders(token); 
+      setAuthHeaders(token);
 
-      const response = await axios.post(API_URL, newRecord); 
-      return response.data; 
+      const response = await axios.post(API_URL, newRecord);
+      return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(
         error.response?.data || "Failed to add water record"
@@ -55,18 +52,17 @@ export const addWaterRecord = createAsyncThunk(
   }
 );
 
-
 export const deleteWaterRecord = createAsyncThunk(
   "dailyInfo/deleteWaterRecord",
   async (id, thunkAPI) => {
     try {
-      const state = thunkAPI.getState(); 
-      const token = state.auth.token; 
+      const state = thunkAPI.getState();
+      const token = state.auth.token;
 
-      setAuthHeaders(token); 
+      setAuthHeaders(token);
 
-      await axios.delete(`${API_URL}/${id}`); 
-      return id; 
+      await axios.delete(`${API_URL}/${id}`);
+      return id;
     } catch (error) {
       return thunkAPI.rejectWithValue(
         error.response?.data || "Failed to delete water record"
@@ -75,18 +71,19 @@ export const deleteWaterRecord = createAsyncThunk(
   }
 );
 
-
 export const updateWaterRecord = createAsyncThunk(
   "dailyInfo/updateWaterRecord",
   async ({ id, updatedRecord }, thunkAPI) => {
+    console.log(id, updatedRecord);
+
     try {
-      const state = thunkAPI.getState(); 
-      const token = state.auth.token; 
+      const state = thunkAPI.getState();
+      const token = state.auth.token;
 
-      setAuthHeaders(token); 
+      setAuthHeaders(token);
 
-      const response = await axios.put(`${API_URL}/${id}`, updatedRecord); 
-      return response.data; 
+      const response = await axios.put(`${API_URL}/${id}`, updatedRecord);
+      return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(
         error.response?.data || "Failed to update water record"
@@ -94,4 +91,3 @@ export const updateWaterRecord = createAsyncThunk(
     }
   }
 );
-
